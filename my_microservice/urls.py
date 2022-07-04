6e538eef3be8cd03a14c9b1b3b78bf1370324c8a
omerSuliman <omerahmed41@gmail.com>
2022-07-04 14:33:04 +0400

    update get_schema_view info

--- my_microservice/urls.py
+++ my_microservice/urls.py
@@ -14,12 +14,12 @@ router.register(r'groups', views.GroupViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="OPEN API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(github="https://github.com/omerahmed41/"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
