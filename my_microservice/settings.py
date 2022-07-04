e515f282e8b1de737baa56b2d0cd92280f186ef6
omerSuliman <omerahmed41@gmail.com>
2022-07-04 10:29:26 +0400

    intsall djangorestframework, create app user

--- my_microservice/settings.py
+++ my_microservice/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'user',
+
+    # packages
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
